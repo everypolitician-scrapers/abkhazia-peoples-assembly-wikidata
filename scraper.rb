94b87afe9f8a4a299953d8af3829728896c7a84c
Tony Bowden <tony@tmtm.com>
2019-04-24 07:58:23 +0100

    tidy spacing

--- scraper.rb
+++ scraper.rb
@@ -3,25 +3,24 @@
 
 require 'wikidata/fetcher'
 
-en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+en_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: "https://en.wikipedia.org/wiki/5th_convocation_of_the_People's_Assembly_of_Abkhazia",
   xpath: '//table[.//th[contains(., "Name")]]//tr[td]//td[3]//a[not(@class="new")]/@title'
-) 
+)
 
-en_names2 = EveryPolitician::Wikidata.wikipedia_xpath( 
+en_names2 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: "https://en.wikipedia.org/wiki/5th_convocation_of_the_People's_Assembly_of_Abkhazia",
   xpath: '//table[.//th[contains(., "Name")]]//tr[td]//td[1]//a[not(@class="new")]/@title'
-) 
+)
 
-ab_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+ab_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ab.wikipedia.org/wiki/Аҧсны_Жәлар_Реизара',
   xpath: '//table[.//th[contains(., "Адепутат")]]//tr[td]//td[3]//a[not(@class="new")]/@title',
-) 
+)
 
-ru_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+ru_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ru.wikipedia.org/wiki/Народное_собрание_Республики_Абхазия',
   xpath: '//table[.//th[contains(., "Депутат")]]//tr[td]//td[3]//a[not(@class="new")]/@title',
-) 
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names | en_names2, ab: ab_names, ru: ru_names })
-
